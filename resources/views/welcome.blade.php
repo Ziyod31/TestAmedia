9f9c547bbb14f697795a6187d607263f24049670
Ziyoviddin <ziyod_31@mail.ru>
2022-04-23 13:28:00 +0500

    beginning

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>File</th>
+                        <th>Message</th>
+                        <th>Status</th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </tbody>
+    </div>
+</div>
+</div>/
+@endsection
